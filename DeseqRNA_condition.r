633ca773d6d04111a8c5b0ced4df1e832287881e
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2023-03-19 20:03:44 -0700

    Update DeseqRNA_condition.r

--- DeseqRNA_condition.r
+++ DeseqRNA_condition.r
@@ -1,10 +1,10 @@
+library(DESeq2)
 metadata <- read.delim("metadata.csv", header = TRUE, sep = ',', row.names=1)
 
 countsData <- read.csv("data_t.csv", header = TRUE, row.names = 1, sep = ',', check.names = FALSE)
 
 countsData <- t(countsData)
 head(countsData)
-#countsData <- countsData[, -1]
 
 ncol(countsData)
 
@@ -17,8 +17,6 @@ colData <- DataFrame(condition=factor(metadata$condition))
 
 dds <- DESeqDataSetFromMatrix(ceiling(countsData), colData = colData, design=formula(~condition)) #gets stuck here
 
-#contrast = c("strain", "C57BL/6T", "BALB/cT")
-#contrast = c("libprep", "ribodepleted", "polyA")
 contrast = c("condition", "FLT", "GC")
 
 dds <- DESeq(dds)
@@ -26,6 +24,16 @@ res <- results(dds, contrast = contrast)
 
 resOrdered <- res[order(res$padj), ]
 resOrdered
+
+summary(resOrdered)
+
+#out of 33067 with nonzero total read count
+#adjusted p-value < 0.1
+#LFC > 0 (up)       : 169, 0.51%
+#LFC < 0 (down)     : 81, 0.24%
+#outliers [1]       : 0, 0%
+#low counts [2]     : 10911, 33%
+#(mean count < 1)
 
 pVal <- 0.05
 l2fc <- 2
@@ -49,7 +57,6 @@ geneIDs <- mapIds(org.Mm.eg.db, keys=row.names(significant), column="SYMBOL", ke
 
 geneIDs
 
-
 #Gene Results
 
 #condition
@@ -59,4 +66,28 @@ geneIDs
 #            "Myh8"             "Tff3"           "Mybpc2"
 #ENSMUSG00000030324 ENSMUSG00000058975
 #             "Rho"            "Kcnc1"
+
+# Effects of transformations on the variance
+# this gives log2(n + 1)
+ntd <- normTransform(dds)
+library("vsn")
+meanSdPlot(assay(ntd))
+
+vsd <- vst(dds, blind=FALSE)
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+library("RColorBrewer")
+
+plotPCA(vsd, intgroup=c("condition"))
+
+select <- order(rowVars(assay(vsd)), decreasing = TRUE)[1:25]
+
+pheatmap(assay(vsd)[select,],
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         scale = "row",
+         fontsize = 8,
+         show_colnames = TRUE,
+         main = "Heatmap of VST-transformed data - condition")
+
 
